d793615db8585c681e642baf1456c176a19e1cde
Muthu4558 <muthukdm45@gmail.com>
2025-12-24 09:46:22 +0530

    checking mail on render

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -19,8 +19,8 @@ import nodemailer from "nodemailer";
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: 'muthukdm45@gmail.com',
-      pass:  'rrnt eaca hfvu btgv',
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
   });
 
